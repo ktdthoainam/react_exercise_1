31d63c5e4e7fb1aae5f3ae44ffad1e0de5fbd197
ktdthoainam <ktdthoainam@gmail.com>
2023-09-21 17:11:19 +0700

    add function count_prime

--- script.js
+++ script.js
@@ -2,11 +2,27 @@
 let a;
 let sum=0;
 let evens =0;
+let count=0;
 function random_number(){
 a = Math.floor(Math.random() * 51) + 10;
 return a;
 }
 
+function check_prime(){
+  for( let j=2;j<a;j++ ){
+    if(a%j==0){
+      return false;
+    }
+  }
+  return true;
+}
+function count_prime()
+{
+  if(check_prime(true)){
+    return count++;
+  }
+  return count;
+}
 function sum_numbers(){
 sum +=a;
 return sum;
@@ -24,8 +40,10 @@ random_number();
 Array = array.push(a);
 sum_numbers();
 count_evens();
+count_prime();
  }
 document.write(`<pre>mảng bao gồm 6 giá trị được tạo ngẫu nhiên trong [10,60]: Array = [${array}]`);
-document.write(`<pre>tổng các giá trị của chuỗi là:${sum}`)
 document.write(`<pre>số phần tử chẵn là ${evens}`);
+document.write(`<pre>tổng các giá trị của chuỗi là:${sum}`)
+document.write( `<pre>số nguyên tố có trong mảng : ${count}`)
 
